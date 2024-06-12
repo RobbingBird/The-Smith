14eb28d515549020e507642191fce024280734c0
RobbingBird <Sloofrobbert@gmail.com>
2024-06-12 14:44:25 +0200

    Cutscene1

--- The Smith/Assets/Scripts/Dialogue.cs
+++ The Smith/Assets/Scripts/Dialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
@@ -9,20 +10,49 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI swordInput;
     public GameObject player;
     private string swordName = "sword";
-    public TextMeshProUGUI nameDisplay;
+    public PlayableDirector dialogue;
 
-    public void changeSwordName(){
-        swordName = swordInput.text;
-        nameDisplay.text = swordName;
+    void Start()
+    {
+        inputDisable();
+
+        // Subscribe to the stopped event
+        if (dialogue != null)
+        {
+            dialogue.stopped += OnPlayableDirectorStopped;
+        }
     }
 
-    public void inputDisable(){
+    void OnDestroy()
+    {
+        // Unsubscribe from the stopped event when the script is destroyed
+        if (dialogue != null)
+        {
+            dialogue.stopped -= OnPlayableDirectorStopped;
+        }
+    }
+
+    // This method will be called when the timeline stops playing
+    private void OnPlayableDirectorStopped(PlayableDirector director)
+    {
+        inputEnable();  // Enable player input after the timeline ends
+    }
+
+    public void changeSwordName()
+    {
+        swordName = swordInput.text;
+        Debug.Log(swordName);
+    }
+
+    public void inputDisable()
+    {
         player.GetComponent<Movement>().enabled = false;
         player.GetComponent<ForgeInteract>().enabled = false;
     }
 
-    public void inputEnable(){
-        player.GetComponent <Movement>().enabled = true;
+    public void inputEnable()
+    {
+        player.GetComponent<Movement>().enabled = true;
         player.GetComponent<ForgeInteract>().enabled = true;
     }
 }
